ce7c8e942734205a12d00aa965d49469eb8d25d7
mananwason <manan13056@iiitd.ac.in>
2015-04-18 03:27:10 +0530

    screenshot capability in displayresult working

--- app/src/main/java/evsapp/sid/com/evsapp/DisplayResult.java
+++ app/src/main/java/evsapp/sid/com/evsapp/DisplayResult.java
@@ -3,8 +3,10 @@ package evsapp.sid.com.evsapp;
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.method.ScrollingMovementMethod;
@@ -25,6 +27,9 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.HashMap;
 
@@ -39,6 +44,8 @@ public class DisplayResult extends ActionBarActivity {
     Toolbar mToolbar;
     TableLayout tableLayout;
     TableRow.LayoutParams params;
+    String filePath = Environment.getExternalStorageDirectory()
+            + File.separator + "Pictures/screenshot.png";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -148,7 +155,8 @@ public class DisplayResult extends ActionBarActivity {
     private Bitmap takeScreenshot() {
         View rootView = findViewById(android.R.id.content).getRootView();
         rootView.setDrawingCacheEnabled(true);
-        return rootView.getDrawingCache();
+        mDataBitmap= rootView.getDrawingCache();
+        return null;
     }
 
     @Override
@@ -175,16 +183,42 @@ public class DisplayResult extends ActionBarActivity {
                 return true;
             // case R.id.action_share_item : shareDataImage();return true;
             case R.id.action_share_data:
-                Intent share = new Intent();
+                takeScreenshot();
+                saveBitmap(mDataBitmap);
+                send(filePath);
+              /*  Intent share = new Intent();
                 share.setAction(Intent.ACTION_SEND);
                 share.putExtra(Intent.EXTRA_TEXT, "share app link or some text");
                 share.setType(HTTP.PLAIN_TEXT_TYPE);
                 if (share.resolveActivity(getPackageManager()) != null) {
                     startActivity(share);
                 }
-
+*/
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    public void saveBitmap(Bitmap bitmap) {
+
+        File imagePath = new File(filePath);
+        FileOutputStream fos;
+        try {
+            fos = new FileOutputStream(imagePath);
+            bitmap.compress(Bitmap.CompressFormat.PNG, 100, fos);
+            fos.flush();
+            fos.close();
+        } catch (FileNotFoundException e) {
+            Log.e("GREC", e.getMessage(), e);
+        } catch (IOException e) {
+            Log.e("GREC", e.getMessage(), e);
+        }
+    }
+
+    public void send(String path) {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("image/png");
+        Uri myUri = Uri.parse("file://" + path);
+        intent.putExtra(Intent.EXTRA_STREAM, myUri);
+        startActivity(Intent.createChooser(intent, "Share data"));
     }
 }
