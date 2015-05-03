e2c22ef8ead902df2683a347f7eedb697926ddda
mananwason <manan13056@iiitd.ac.in>
2015-05-04 04:57:06 +0530

    cardview about us

--- app/src/main/java/evsapp/sid/com/evsapp/AboutUs.java
+++ app/src/main/java/evsapp/sid/com/evsapp/AboutUs.java
@@ -1,23 +1,54 @@
 package evsapp.sid.com.evsapp;
 
 import android.app.Activity;
-import android.graphics.Bitmap;
 import android.os.Bundle;
-import android.support.v7.widget.RecyclerView;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageView;
-import android.widget.TextView;
 
-import com.mikepenz.aboutlibraries.Libs;
+import java.util.ArrayList;
 
-import com.mikepenz.aboutlibraries.Libs;
+import it.gmariotti.cardslib.library.internal.Card;
+import it.gmariotti.cardslib.library.internal.CardArrayAdapter;
+import it.gmariotti.cardslib.library.internal.CardHeader;
+import it.gmariotti.cardslib.library.internal.CardThumbnail;
+import it.gmariotti.cardslib.library.view.CardListView;
 
 
 public class AboutUs extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
 
-    public class ContactInfo {
+        int listImages[] = new int[]{R.drawable.manan, R.drawable.me,
+                R.drawable.steve, R.drawable.bill, R.drawable.mark};
+
+        ArrayList<Card> cards = new ArrayList<Card>();
+
+        for (int i = 0; i<5; i++) {
+            // Create a Card
+            Card card = new Card(this);
+            // Create a CardHeader
+            CardHeader header = new CardHeader(this);
+            // Add Header to card
+            header.setTitle("Developer: " + i);
+            card.setTitle("Android");
+            card.addCardHeader(header);
+
+            CardThumbnail thumb = new CardThumbnail(this);
+            thumb.setDrawableResource(listImages[i]);
+            card.addCardThumbnail(thumb);
+
+            cards.add(card);
+        }
+
+        CardArrayAdapter mCardArrayAdapter = new CardArrayAdapter(this, cards);
+
+        CardListView listView = (CardListView) this.findViewById(R.id.myList);
+        if (listView != null) {
+            listView.setAdapter(mCardArrayAdapter);
+        }
+    }
+   /* public class ContactInfo {
         protected String name;
         protected String surname;
         protected String email;
@@ -76,5 +107,5 @@ public class AboutUs extends Activity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
+    }*/
 }
