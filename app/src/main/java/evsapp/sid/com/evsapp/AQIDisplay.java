c9f69305a96dc566e4c2149527b5a553871829ae
mananwason <manan13056@iiitd.ac.in>
2015-05-04 07:01:02 +0530

    cleanup

--- app/src/main/java/evsapp/sid/com/evsapp/AQIDisplay.java
+++ app/src/main/java/evsapp/sid/com/evsapp/AQIDisplay.java
@@ -1,8 +1,13 @@
 package evsapp.sid.com.evsapp;
 
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
 /**
  * Created by manan on 04-05-2015.
  */
-public class AQIDisplay {
+public class AQIDisplay extends ActionBarActivity{
+
+    
 
 }
